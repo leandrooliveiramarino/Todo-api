924b6e8cc34d8c0714052ce35282cc5230e34d3f
= <=>
2016-03-26 19:00:21 -0300

    Add postgres for production

--- db.js
+++ db.js
@@ -1,4 +1,6 @@
 var Sequelize = require('sequelize');
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
 var sequelize = new Sequelize(undefined, undefined, undefined, {
 	'dialect': 'sqlite',
 	'storage': __dirname + '/data/dev-todo-api.sqlite'
