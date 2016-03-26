bf021136b1571898da3e2be25bdbe79979a8835b
= <=>
2016-03-26 01:52:41 -0300

    underscore adicionado para validação dos dados

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -16,12 +17,13 @@ app.get('/todos', function(req, res){
 app.get('/todos/:id', function(req, res){
 	var todoId = parseInt(req.params.id);
 	var matchedTodo;
-	todos.forEach(function(element){
+	_.findWhere(todos, {id: todoId});
+	/*todos.forEach(function(element){
 		if(element.id === todoId) {
 			matchedTodo = element;
 			return;
 		}
-	});
+	});*/
 
 	if(!matchedTodo) {
 		res.status(404).send();
@@ -33,8 +35,15 @@ app.get('/todos/:id', function(req, res){
 
 // POST /todos
 app.post('/todos', function(req, res){
-	var body = req.body;
+	var body = _.pick(req.body, "description", "completed");
+	//Use _.pick to only pick description and completed
 
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		res.status(400).send();
+	}
+
+	//set body.description to trimmed value
+	body.description = body.description.trim();
 	body.id = todoNextId++;
 
 	todos.push(body);
