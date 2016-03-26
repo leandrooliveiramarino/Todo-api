55aab29c767e39e9fb89f44d2976b70281ae337a
= <=>
2016-03-26 02:39:28 -0300

    Add delete todos route

--- server.js
+++ server.js
@@ -16,14 +16,7 @@ app.get('/todos', function(req, res){
 // GET /todos/:id
 app.get('/todos/:id', function(req, res){
 	var todoId = parseInt(req.params.id);
-	var matchedTodo;
-	_.findWhere(todos, {id: todoId});
-	/*todos.forEach(function(element){
-		if(element.id === todoId) {
-			matchedTodo = element;
-			return;
-		}
-	});*/
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
 	if(!matchedTodo) {
 		res.status(404).send();
@@ -50,6 +43,18 @@ app.post('/todos', function(req, res){
 	
 	console.log('description: ' + body.description);
 	res.json(body);
+});
+
+app.delete('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if(!matchedTodo)
+		res.status(404).json({"error": "no todo found with that id"});
+	else {
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
 });
 
 app.listen(PORT, function(){
