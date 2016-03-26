951fcf1b0afa96be13e387da28f2a41ab82c7322
= <=>
2016-03-26 12:33:35 -0300

    Changing some existing data with the put method

--- server.js
+++ server.js
@@ -57,6 +57,39 @@ app.delete('/todos/:id', function(req, res){
 	}
 });
 
+//PUT /todos/:id
+app.put('/todos/:id', function(req, res){
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if(!matchedTodo) {
+		return res.status(404).send();
+	}
+
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if(body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	} else {
+		//res.json({'error':'Something went wrong on the completed field'});
+	}
+
+	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if(body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	} else {
+		res.json({'error':'Something went wrong on the description field'});
+	}
+
+	_.extend(matchedTodo, validAttributes);
+
+	res.json(matchedTodo);
+
+});
+
 app.listen(PORT, function(){
-	console.log('Express listening on port ' + PORT +'!');
+	console.log('Express listening on port ' + PORT + '!');
 });
