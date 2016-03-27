24c8ffb0311da5967adc790e1631d4780a62006c
= <=>
2016-03-26 23:08:48 -0300

    Convert email to lower case

--- models/user.js
+++ models/user.js
@@ -15,6 +15,14 @@ module.exports = function(sequelize, DataTypes){
 				len: [7,100]
 			}
 		}
+	}, {
+		hooks: {
+			beforeValidate: function(user, options) {
+				//user.email and convert to a lower case if it is a string
+				if(typeof user.email === 'string')
+					user.email = user.email.toLowerCase();
+			}
+		}
 	})
 
 }
